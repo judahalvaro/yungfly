7b8adafee07f5e31e8b2a24df7195041c949d2d7
judah alvaro <judahalvaro123@gmail.com>
2024-02-29 22:28:14 +0000

    XORinClassProject.py

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -17,3 +17,82 @@ def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
 
+
+
+def XOR(bit1, bit2):
+
+    if bit1 ==bit2:
+        return '0'
+    else:
+        return '1'
+
+
+
+
+
+def XORonByte(byte, key):
+
+    emsg = ""
+
+    for i in range(len(byte)):
+        emsg += XOR(byte[i], key[i])
+
+    return emsg
+
+print(XORonByte("0010", "0011"))
+
+def XORonLetter(letter, keyLetter):
+
+    letterBin = encode(letter)
+    keyLetterBin = encode(keyLetter)
+
+    encryptedLetter = XORonByte(letterBin, keyLetterBin)
+
+    return decode(encryptedLetter)
+
+print(XORonLetter("d", "r"))
+
+def XORonSentence(sentence, key):
+
+    encryptedSentence = ""
+
+    for i in range(len(sentence)):
+
+        encryptedSentence += XORonLetter(sentence[i], key[i])
+
+    return encryptedSentence
+def generateKey(message, key):
+
+    if len(message) ==len(key):
+
+        return key
+
+
+    elif len(message) < len(key):
+
+            return key[0:len(message)]
+
+    else:
+        genKey = ""
+        rep = math.floor(len(message/len(key)))
+        rem = len(message) % len(key)
+
+        for i in range(rep):
+            genKey += 1
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(XORonSentence("hello", "world"))
+print(XORonSentence("rkAan", "world"))
